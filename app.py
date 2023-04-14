57706764de46a7828c9303f71150106c912202d7
mannadurai1 <mannadurai1@toromail.csudh.edu>
2023-04-13 19:55:31 -0700

    debugging

--- app.py
+++ app.py
@@ -20,4 +20,26 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World!'
+    return "Test"
+    
+@app.route('/test')
+def test():
+    value1 = Flask.request.args.get('val1')
+    value2 = Flask.request.args.get('val2')
+    return "Test Results:" + value1 + value2
+
+    
+'''
+@app.route('/', methods=['GET', 'POST'])
+def home():
+   return Flask.render_template('index.html')
+
+@app.route('/get_numbers')
+def get_values():
+   value1 = Flask.request.args.get('val1')
+   value2 = Flask.request.args.get('val2')
+   return Flask.jsonify({'data':f'<p>The result is: {value1+value2}</p>'})
+
+
+
+'''
